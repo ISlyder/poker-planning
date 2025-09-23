28bdd9a6b6e74d0da9ea8a64d5afc46df607a64d
Clement <duvivier.clement@gmail.com>
2025-09-23 23:09:05 +0200

    Update App layout and branding with logo, footer, and metadata adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
 import PokerPlanning from "./vote/poker-planning";
+import logo from './assets/main-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="bg-primary text-white p-4">
-        <h1 className="text-2xl font-bold">🃏 Poker planning</h1>
+    <div className="App flex flex-col h-screen justify-between">
+      <header className="bg-primary text-white p-4 flex gap-1">
+          <img src={logo} className={"h-10 w-10"} alt={"Logo"}/>
+        <h1 className="text-2xl font-bold">Poker planning Infra&IOT Team</h1>
       </header>
-       <PokerPlanning />
+        <main className="mb-auto">
+            <PokerPlanning />
+        </main>
+        <footer className={"w-full bg-primary text-white p-4 mt-8 static bottom-0"}>
+            <p>Developped By Clément</p>
+        </footer>
     </div>
   );
 }
