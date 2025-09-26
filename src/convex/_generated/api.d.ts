e614f4f9ca44badca49f4865ba944dc8a79f7151
Clement <iceslyder@gmail.com>
2025-09-26 19:22:41 +0200

    Integrate Convex for voting functionality and update Poker Planning component

--- src/convex/_generated/api.d.ts
+++ src/convex/_generated/api.d.ts
@@ -13,7 +13,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as functions_vote from "../functions/vote.js";
+import type * as functions_votes from "../functions/votes.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -24,7 +24,7 @@ import type * as functions_vote from "../functions/vote.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  "functions/vote": typeof functions_vote;
+  "functions/votes": typeof functions_votes;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
